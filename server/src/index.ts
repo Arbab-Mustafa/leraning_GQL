bbdd8da3c426387b537a3e38842acdbf0cb510f0
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2025-01-22 23:12:52 +0500

    addes

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// Sample route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Node.js!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running http://localhost:${PORT}`);
+});
