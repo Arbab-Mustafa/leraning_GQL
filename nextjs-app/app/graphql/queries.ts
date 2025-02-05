d02a8e0e7241255c2210bd92af084d8ed83b7954
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2025-02-05 20:41:52 +0500

    added frontend Ui

--- nextjs-app/app/graphql/queries.ts
+++ nextjs-app/app/graphql/queries.ts
@@ -19,3 +19,38 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const GET_COURSES = gql`
+  query getCourses {
+    getCourses {
+      id
+      title
+    }
+  }
+`;
+
+export const ADD_COURSE = gql`
+  mutation addCourse($title: String!) {
+    addCourse(title: $title) {
+      title
+    }
+  }
+`;
+
+export const GET_TEACHERS = gql`
+  query GetTeachers {
+    getTeachers {
+      id
+      name
+      courses {
+        id
+        title
+      }
+
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -2,15 +2,34 @@
 "use client";
 
 import { useQuery, useMutation } from "@apollo/client";
-import { GET_USERS, ADD_USER } from "./graphql/queries";
+import {
+  GET_USERS,
+  ADD_USER,
+  GET_COURSES,
+  ADD_COURSE,
+  GET_TEACHERS,
+} from "./graphql/queries";
 import { useState } from "react";
 
 export default function Home() {
   const { data, loading, error } = useQuery(GET_USERS);
+  const {
+    data: coursesData,
+    loading: coursesLoading,
+    error: coursesError,
+  } = useQuery(GET_COURSES);
+
+  const {
+    data: teachersData,
+    loading: teachersLoading,
+    error: teachersError,
+  } = useQuery(GET_TEACHERS);
   const [addUser] = useMutation(ADD_USER);
+  const [addCourse] = useMutation(ADD_COURSE);
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [title, setTitle] = useState("");
 
   const handleAddUser = async () => {
     try {
@@ -21,6 +40,18 @@ export default function Home() {
       console.error("Error adding user:", err);
     }
   };
+
+  const handleCourse = async (event: any) => {
+    try {
+      event.preventDefault();
+      await addCourse({ variables: { title } });
+      setTitle("");
+    } catch (error) {
+      console.error("Error adding course:", error);
+    }
+  };
+
+  const handleTeacher = async (event: any) => {};
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -56,6 +87,61 @@ export default function Home() {
         >
           Add User
         </button>
+      </div>
+
+      <div>
+        <h1 className="text-2xl font-bold mb-4 mt-8">Courses</h1>
+        {coursesLoading ? (
+          <p>Loading...</p>
+        ) : coursesError ? (
+          <p>Error: {coursesError.message}</p>
+        ) : (
+          <ul>
+            {coursesData?.getCourses.map((course: any) => (
+              <li key={course.id} className="border p-2 mb-2 rounded">
+                {course.title}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <div>
+        <form>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+            className="border p-2 rounded"
+          />
+          <button onClick={handleCourse}>Add Course</button>
+        </form>
+      </div>
+
+      {/* teacher  */}
+
+      <div>
+        <h2>Teachers</h2>
+
+        {teachersLoading ? (
+          <p>Loading...</p>
+        ) : teachersError ? (
+          <p>Error: {teachersError.message}</p>
+        ) : (
+          <ul>
+            {teachersData?.getTeachers.map((teacher: any) => (
+              <li key={teacher.id} className="border p-2 mb-2 rounded">
+                {teacher.name}
+              </li>
+            ))}
+          </ul>
+        )}
+
+        <form>
+          <input type="text" placeholder="Name" />
+          <button onClick={handleTeacher}> </button>
+        </form>
       </div>
     </main>
   );
