716815a7233405d131b88f69830f7d6d7b259d1d
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2025-02-06 23:23:24 +0500

    frontend Ui

---
+++ nextjs-app/app/animal/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import { useQuery, useMutation } from "@apollo/client";
+
+const AnimalPage = () => {
+  return <div>AnimalPage</div>;
+};
+
+export default AnimalPage;
