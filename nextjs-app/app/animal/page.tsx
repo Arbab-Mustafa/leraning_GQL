cdd5d95ba0af436c8e6f0ce1ec33611e073d9084
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2025-02-06 23:47:12 +0500

    apdate automatically cache of graphql to fetch data

--- nextjs-app/app/animal/page.tsx
+++ nextjs-app/app/animal/page.tsx
@@ -1,10 +1,83 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
+import { GET_ANIMALS, ADD_ANIMAL } from "../graphql/queries";
+
+interface Animal {
+  id: string;
+  name: string;
+  type: string;
+}
 
 const AnimalPage = () => {
-  return <div>AnimalPage</div>;
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+
+  const { data, loading, error } = useQuery(GET_ANIMALS);
+  const [addAnimal] = useMutation(ADD_ANIMAL, {
+    update(cache, { data: { addAnimal } }) {
+      // Read the current data from the cache
+      const existingAnimals = cache.readQuery<{ getAnimals: Animal[] }>({
+        query: GET_ANIMALS,
+      });
+
+      // Write the new data back to the cache
+      if (existingAnimals) {
+        cache.writeQuery({
+          query: GET_ANIMALS,
+          data: {
+            getAnimals: [...existingAnimals.getAnimals, addAnimal],
+          },
+        });
+      }
+    },
+  });
+  const handleAnimal = async (event: any) => {
+    event.preventDefault();
+    try {
+      await addAnimal({ variables: { name, type } });
+
+      setName("");
+      setType("");
+    } catch (error) {
+      console.error("Error adding animal:", error);
+    }
+  };
+
+  useEffect(() => {}, []);
+
+  return (
+    <div>
+      <h2>Animal</h2>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data &&
+        data.getAnimals.map((animal: Animal) => (
+          <div key={animal.id}>
+            <h3>{animal.name}</h3>
+          </div>
+        ))}
+      {/*  */}
+
+      <form>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Type"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        />
+        <button onClick={handleAnimal}>Add Animal</button>
+      </form>
+    </div>
+  );
 };
 
 export default AnimalPage;
