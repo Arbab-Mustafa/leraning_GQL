2ffdedd48d1ca4a10543a427ce68bbf116be64c5
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2025-02-11 00:36:11 +0500

    fix issue

--- reactjs-app/server/src/index.ts
+++ reactjs-app/server/src/index.ts
@@ -1,1 +1,33 @@
- 
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `
+
+type Query{
+    hello: String
+
+}
+
+
+
+`;
+const resolvers = {
+  Query: {
+    hello: () => "Hello World",
+  },
+};
+
+const Apollo_Server = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+};
+
+Apollo_Server();
